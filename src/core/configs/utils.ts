9813b9094f2816aa0fe6ad050b03f98b4e1ad99e
Jesse Ikemefuna <jesseikemefuna@gmail.com>
2025-11-17 22:50:40 +0100

    hotfix 15

--- src/core/configs/utils.ts
+++ src/core/configs/utils.ts
@@ -18,7 +18,20 @@ import { jobApplicationModel } from "../../features/jobApplication/models/jobApp
 import { ObjectId } from "mongoose";
 import { initializeApp, cert } from "firebase-admin/app";
 import { Message, getMessaging } from "firebase-admin/messaging";
-var serviceAccount = require("../../../assets/push-notif-key.json");
+// var serviceAccount = require("../../../assets/push-notif-key.json");
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"), // Handle newlines
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.FIREBASE_CERT_URL,
+} as any;
 
 const app = initializeApp({
   credential: cert(serviceAccount),
