f4e90aa7a32e7a58d7d2b1adc526b6f3f338830f
Jesse Ikemefuna <jesseikemefuna@gmail.com>
2025-11-18 19:24:44 +0100

    Vercel setup

---
+++ api/index.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+import createApp from "../src/app.ts";
+
+import connectMongo from "../src/core/configs/connectMongo.ts";
+
+// Initialize the database connection
+const db = connectMongo();
+
+// Create the Express app
+const app = createApp(db);
+
+// Export for Vercel serverless
+export default app;
--- src/core/configs/connectMongo.ts
+++ src/core/configs/connectMongo.ts
@@ -1,12 +1,20 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectMongo = async () => {
+  if (isConnected) {
+    console.log("Using existing DB connection");
+    return mongoose.connection;
+  }
   try {
     const mongoUri =
       process.env.MONGO_URI || "mongodb://localhost:27017/legwork";
     await mongoose.connect(mongoUri);
+    isConnected = true;
 
     console.log("Connected to MongoDB successfully");
+    return mongoose.connection
   } catch (error) {
     console.log("Error connecting to MongoDB:", error);
   }
